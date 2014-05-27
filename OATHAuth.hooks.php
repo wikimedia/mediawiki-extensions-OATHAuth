1a8006317dd2c52e4f70d10f585800e8efeb5b1a
Tyler Anthony Romeo <tylerromeo@gmail.com>
2014-05-27 14:00:21 -0400

    Move token login to separate page
    Rather than have an extraneous form on the login page,
    move the token input to a separate page. The actual
    logic for logging in is identical, the only difference
    is that the token is added to the form data on a second
    page request.
    
    Bug: 53195
    Change-Id: I39859cc59f1811de42b72f6167d332ea48812f97

--- OATHAuth.hooks.php
+++ OATHAuth.hooks.php
@@ -7,23 +7,6 @@
  */
 class OATHAuthHooks {
 	/**
-	 * @param $template UserloginTemplate
-	 * @return bool
-	 */
-	static function ModifyUITemplate( &$template ) {
-		$input = '<div><label for="wpOATHToken">'
-			. wfMessage( 'oathauth-token' )->escaped()
-			. '</label>'
-			. Html::input( 'wpOATHToken', null, 'text', array(
-					'class' => 'loginText', 'id' => 'wpOATHToken', 'tabindex' => '3', 'size' => '20'
-				) ) . '</div>';
-
-		$template->set( 'extrafields', $template->get( 'extrafields', '' ) . $input );
-
-		return true;
-	}
-
-	/**
 	 * Get the singleton OATH user repository
 	 *
 	 * @return OATHUserRepository
@@ -59,7 +42,19 @@ class OATHAuthHooks {
 	 * @return bool
 	 */
 	static function AbortChangePassword( $user, $password, $newpassword, &$errorMsg ) {
-		$result = self::authenticate( $user );
+		global $wgRequest;
+
+		$token = $wgRequest->getText( 'wpOATHToken' );
+		$oathrepo = self::getOATHUserRepository();
+		$oathuser = $oathrepo->findByUser( $user );
+		# Though it's weird to default to true, we only want to deny
+		# users who have two-factor enabled and have validated their
+		# token.
+		$result = true;
+
+		if ( $oathuser->getKey() !== null ) {
+			$result = $oathuser->getKey()->verifyToken( $token, $oathuser );
+		}
 
 		if ( $result ) {
 			return true;
@@ -78,57 +73,18 @@ class OATHAuthHooks {
 	 * @return bool
 	 */
 	static function AbortLogin( $user, $password, &$abort, &$errorMsg ) {
-		$result = self::authenticate( $user );
-		if ( $result ) {
-			return true;
-		} else {
-			$abort = LoginForm::ABORTED;
-			$errorMsg = 'oathauth-abortlogin';
-			return false;
-		}
-	}
+		$context = RequestContext::getMain();
+		$request = $context->getRequest();
+		$output = $context->getOutput();
 
-	/**
-	 * @param $user User
-	 * @return bool
-	 */
-	static function authenticate( $user ) {
-		global $wgRequest;
+		$oathrepo = self::getOATHUserRepository();
+		$oathuser = $oathrepo->findByUser( $user );
 
-		$token = $wgRequest->getText( 'wpOATHToken' );
-		$oathuser = self::getOATHUserRepository()->findByUser( $user );
-		# Though it's weird to default to true, we only want to deny
-		# users who have two-factor enabled and have validated their
-		# token.
-		$result = true;
-
-		if ( $oathuser->getKey() !== null ) {
-			$result = $oathuser->getKey()->verifyToken( $token, $oathuser );
-		}
-
-		return $result;
-	}
-
-	/**
-	 * Determine if two-factor authentication is enabled for $wgUser
-	 *
-	 * @param bool &$isEnabled Will be set to true if enabled, false otherwise
-	 *
-	 * @return bool False if enabled, true otherwise
-	 */
-	static function TwoFactorIsEnabled( &$isEnabled ) {
-		global $wgUser;
-
-		$user = self::getOATHUserRepository()->findByUser( $wgUser );
-		if ( $user && $user->getKey() !== null ) {
-			$isEnabled = true;
-			# This two-factor extension is enabled by the user,
-			# we don't need to check others.
+		if ( $oathuser->getKey() !== null && !$request->getCheck( 'token' ) ) {
+			$request->setSessionData( 'oath_login', $request->getValues() );
+			$output->redirect( SpecialPage::getTitleFor( 'OATH' )->getFullURL( '', false, PROTO_CURRENT ) );
 			return false;
 		} else {
-			$isEnabled = false;
-			# This two-factor extension isn't enabled by the user,
-			# but others may be.
 			return true;
 		}
 	}
---
+++ special/SpecialOATHLogin.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ * Special page to log users into two factor authentication
+ */
+class SpecialOATHLogin extends FormSpecialPage {
+	/** @var OATHUser|null */
+	private $OATHUser;
+
+	/** @var LoginForm */
+	private $loginForm;
+
+	/**
+	 * @var string|null The token submitted by the user
+	 */
+	private $token = null;
+
+	/**
+	 * Initialize the OATH user based on the current local User object in the context
+	 *
+	 * @param OATHUser $oathuser
+	 * @param WebRequest $oldRequest
+	 */
+	public function __construct( OATHUser $oathuser, WebRequest $oldRequest ) {
+		Hooks::register( 'AbortLogin', $this );
+		parent::__construct( 'OATH', '', false );
+
+		$this->OATHUser = $oathuser;
+		$this->loginForm = new LoginForm( $oldRequest );
+		$this->loginForm->setContext( $this->getContext() );
+	}
+
+	/**
+	 * Set the page title and add JavaScript RL modules
+	 *
+	 * @param HTMLForm $form
+	 */
+	public function alterForm( HTMLForm $form ) {
+		$form->setMessagePrefix( 'oathauth' );
+		$form->setWrapperLegend( false );
+		$form->getOutput()->setPagetitle( $this->msg( 'oathauth-login' ) );
+		$form->getOutput()->addModules( 'ext.oathauth' );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getDisplayFormat() {
+		return 'vform';
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function requiresUnblock() {
+		return false;
+	}
+
+	/**
+	 * @return array[]
+	 */
+	protected function getFormFields() {
+		return array(
+			'token' => array(
+				'type' => 'text',
+				'default' => '',
+				'label-message' => 'oathauth-entertoken',
+				'name' => 'token',
+				'required' => true
+			),
+			'returnto' => array(
+				'type' => 'hidden',
+				'default' => $this->getRequest()->getVal( 'returnto' ),
+				'name' => 'returnto',
+			),
+			'returntoquery' => array(
+				'type' => 'hidden',
+				'default' => $this->getRequest()->getVal( 'returntoquery' ),
+				'name' => 'returntoquery',
+			)
+		);
+	}
+
+	/**
+	 * Stub function: the only purpose of this form is to add more data into
+	 * the login form
+	 *
+	 * @param array $formData
+	 *
+	 * @return true
+	 */
+	public function onSubmit( array $formData ) {
+		$this->getRequest()->setSessionData( 'oath_login', null );
+		$this->token = $formData['token'];
+
+		return true;
+	}
+
+	public function onSuccess() {
+		$this->loginForm->execute( $this->par );
+	}
+
+	/**
+	 * @param User $user
+	 * @param $password
+	 * @param $abort
+	 * @param $errorMsg
+	 *
+	 * @return bool
+	 */
+	public function onAbortLogin( User $user, $password, &$abort, &$errorMsg ) {
+		$result = $this->OATHUser
+			->getKey()
+			->verifyToken( $this->getRequest()->getVal( 'token' ), $this->OATHUser );
+
+		if ( $result ) {
+			return true;
+		} else {
+			$abort = LoginForm::WRONG_PASS;
+
+			return false;
+		}
+	}
+}
