961dd687b9f724e7b6926d0d702b8da24ced65be
victorbarbu <victorbarbu08@gmail.com>
2015-12-17 23:09:57 +0200

    Upgrade Extension:OATHAuth to the registration system
    Bug: T87949
    Change-Id: Id6217249c11922ab7d25b31375789c40d737600c

--- OATHAuth.php
+++ OATHAuth.php
@@ -12,64 +12,18 @@
  * @license GNU General Public Licence 2.0 or later
  */
 
-if ( !defined( 'MEDIAWIKI' ) ) {
-	echo "This file is an extension to the MediaWiki software and cannot be used standalone.\n";
-	die( 1 );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'OATHAuth' );
+
+	$wgMessagesDirs['OATHAuth'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['OATHAuthAlias'] = __DIR__ . 'OATHAuth.alias.php';
+
+	/* wfWarn(
+		'Deprecated PHP entry point used for OATHAuth extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	); */
+
+	return true;
+} else {
+	die( 'This version of the OATHAuth extension requires MediaWiki 1.25+' );
 }
-
-$wgExtensionCredits['other'][] = array(
-	'path' => __FILE__,
-	'name' => 'OATHAuth',
-	'author' => 'Ryan Lane',
-	'version' => '0.2.1',
-	'url' => 'http://mediawiki.org/wiki/Extension:OATHAuth',
-	'descriptionmsg' => 'oathauth-desc',
-);
-
-/**
- * The number of token windows in each direction that should be valid
- *
- * This tells OATH to accept tokens for a range of $wgOATHAuthWindowRadius * 2 windows
- * (which is effectively ((1 + 2 * $wgOATHAuthWindowRadius) * 30) seconds).
- * This range of valid windows is centered around the current time.
- *
- * The purpose of this configuration variable is to account for differences between
- * the user's clock and the server's clock. However, it is recommended to keep it as
- * low as possible.
- *
- * @var int
- */
-$wgOATHAuthWindowRadius = 4;
-
-$dir = __DIR__ . '/';
-
-$wgMessagesDirs['OATHAuth'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['OATHAuth'] = $dir . 'OATHAuth.i18n.php';
-$wgExtensionMessagesFiles['OATHAuthAlias'] = $dir . 'OATHAuth.alias.php';
-$wgAutoloadClasses['OATHAuthHooks'] = $dir . 'OATHAuth.hooks.php';
-$wgAutoloadClasses['HOTP'] = $dir . 'lib/hotp.php';
-$wgAutoloadClasses['HOTPResult'] = $dir . 'lib/hotp.php';
-$wgAutoloadClasses['Base32'] = $dir . 'lib/base32.php';
-$wgAutoloadClasses['OATHUser'] = $dir . 'OATHUser.php';
-$wgAutoloadClasses['SpecialOATH'] = $dir . 'special/SpecialOATH.php';
-$wgSpecialPages['OATH'] = 'SpecialOATH';
-
-$wgResourceModules['ext.oathauth'] = array(
-	'scripts' => array(
-		'modules/jquery.qrcode.js',
-		'modules/qrcode.js',
-	),
-	'position' => 'top',
-	'localBasePath' => dirname( __FILE__ ),
-	'remoteExtPath' => 'OATHAuth',
-);
-
-$wgHooks['AbortChangePassword'][] = 'OATHAuthHooks::AbortChangePassword';
-$wgHooks['AbortLogin'][] = 'OATHAuthHooks::AbortLogin';
-$wgHooks['UserLoginForm'][] = 'OATHAuthHooks::ModifyUITemplate';
-$wgHooks['ChangePasswordForm'][] = 'OATHAuthHooks::ChangePasswordForm';
-$wgHooks['TwoFactorIsEnabled'][] = 'OATHAuthHooks::TwoFactorIsEnabled';
-$wgHooks['LoadExtensionSchemaUpdates'][] = 'OATHAuthHooks::OATHAuthSchemaUpdates';
-$wgHooks['GetPreferences'][] = 'OATHAuthHooks::manageOATH';
-
-
