31c72b5898bb3026c6a6be9b5bce693f4410b7ee
James D. Forrester <jforrester@wikimedia.org>
2019-05-07 09:59:18 -0700

    build: Upgrade eslint-config-wikimedia 0.12.0, drop grunt-jsonlint
    Change-Id: I13f58204a37fe34b802c1fd481927d1226f77430

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,21 @@
 /* eslint-env node */
 module.exports = function ( grunt ) {
 	var conf = grunt.file.readJSON( 'extension.json' );
+
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
-	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-stylelint' );
+
 	grunt.initConfig( {
 		eslint: {
+			options: {
+				reportUnusedDisableDirectives: true,
+				extensions: [ '.js', '.json' ],
+				cache: true
+			},
 			all: [
-				'*.js',
-				'modules/*.js',
+				'**/*.js{,on}',
+				'!{vendor,node_modules}/**',
 				// Library file
 				'!modules/qrcode.js'
 			]
@@ -21,15 +27,8 @@ module.exports = function ( grunt ) {
 				'!vendor/**'
 			]
 		},
-		banana: conf.MessagesDirs,
-		jsonlint: {
-			all: [
-				'**/*.json',
-				'!node_modules/**',
-				'!vendor/**'
-			]
-		}
+		banana: conf.MessagesDirs
 	} );
-	grunt.registerTask( 'test', [ 'eslint', 'stylelint', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'eslint', 'stylelint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
