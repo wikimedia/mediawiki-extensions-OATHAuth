eff6a0738486e84262c95308681a864703ede96c
Reedy <reedy@wikimedia.org>
2020-12-31 03:48:41 +0000

    Convert to HookHandler
    Bug: T270971
    Change-Id: Idf8dad4872a220624b4355a8a9b5e9a02d0e442c

--- src/Hook/LoadExtensionSchemaUpdates/UpdateTables.php
+++ src/Hook/LoadExtensionSchemaUpdates/UpdateTables.php
@@ -25,7 +25,7 @@ class UpdateTables {
 	 * @return bool
 	 */
 	public static function callback( $updater ) {
-		$dir = dirname( dirname( dirname( __DIR__ ) ) );
+		$dir = dirname( __DIR__, 3 );
 		$handler = new static( $updater, $dir );
 		return $handler->execute();
 	}
@@ -172,8 +172,6 @@ class UpdateTables {
 				);
 			}
 		}
-
-		return true;
 	}
 
 	/**
