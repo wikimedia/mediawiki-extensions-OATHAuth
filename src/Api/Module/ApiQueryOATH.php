f8bce0e016cf542ef1378d2fbada93eae45f2bfd
Umherirrender <umherirrender_de.wp@web.de>
2024-10-25 20:15:40 +0200

    Use type-declaration on api module constructor
    Parent class constructor gets type-declaration in 1145328459
    Remove simple doc-blocks without further information
    
    Change-Id: I1b4dc8d9ca01bd9db2f7c461c12e9dff46d76214

--- src/Api/Module/ApiQueryOATH.php
+++ src/Api/Module/ApiQueryOATH.php
@@ -39,14 +39,9 @@ use Wikimedia\ParamValidator\ParamValidator;
 class ApiQueryOATH extends ApiQueryBase {
 	private OATHUserRepository $oathUserRepository;
 
-	/**
-	 * @param ApiQuery $query
-	 * @param string $moduleName
-	 * @param OATHUserRepository $oathUserRepository
-	 */
 	public function __construct(
-		$query,
-		$moduleName,
+		ApiQuery $query,
+		string $moduleName,
 		OATHUserRepository $oathUserRepository
 	) {
 		parent::__construct( $query, $moduleName, 'oath' );
