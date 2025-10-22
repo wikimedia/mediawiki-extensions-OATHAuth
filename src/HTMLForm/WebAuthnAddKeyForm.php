ad0342161acf3a3ae4bd4c69db8802445821de7b
Sam Reed <sam@reedyboy.net>
2025-10-22 21:24:45 +0100

    Remove usages of OATHAllowMultipleModules
    Bug: T404806
    Change-Id: If6c3932288b5339ce54c93e216b1f03f3e2a9519

--- src/HTMLForm/WebAuthnAddKeyForm.php
+++ src/HTMLForm/WebAuthnAddKeyForm.php
@@ -56,28 +56,25 @@ class WebAuthnAddKeyForm extends OATHAuthOOUIHTMLForm {
 
 		$this->getOutput()->addModules( 'ext.webauthn.register' );
 
-		if ( $this->getConfig()->get( 'OATHAllowMultipleModules' ) ) {
-			$moduleDbKeys = $this->oathUser->getKeysForModule( $this->module->getName() );
+		$moduleDbKeys = $this->oathUser->getKeysForModule( $this->module->getName() );
 
-			$recCodeKeys = [];
-			if ( array_key_exists( 0, $moduleDbKeys ) ) {
-				$objRecoveryCodeKeys = array_shift( $moduleDbKeys );
-				if ( $objRecoveryCodeKeys instanceof RecoveryCodeKeys ) {
-					$recCodeKeys = $objRecoveryCodeKeys->getRecoveryCodeKeys();
-				}
+		$recCodeKeys = [];
+		if ( array_key_exists( 0, $moduleDbKeys ) ) {
+			$objRecoveryCodeKeys = array_shift( $moduleDbKeys );
+			if ( $objRecoveryCodeKeys instanceof RecoveryCodeKeys ) {
+				$recCodeKeys = $objRecoveryCodeKeys->getRecoveryCodeKeys();
 			}
-			$recCodeKeysForDisplay = $this->setKeyDataInSession(
-				'RecoveryCodeKeys',
-				[ 'recoverycodekeys' => $recCodeKeys ]
-			);
-			$recCodeKeysForContent = $this->getRecoveryCodesForDisplay( $recCodeKeysForDisplay );
-			$this->getOutput()->addModules( 'ext.oath.recovery' );
-			$this->getOutput()->addModuleStyles( 'ext.oath.recovery.styles' );
-			$this->setOutputJsConfigVars( $recCodeKeysForContent );
-			$html .= $this->generateRecoveryCodesContent( $recCodeKeysForContent, true );
 		}
+		$recCodeKeysForDisplay = $this->setKeyDataInSession(
+			'RecoveryCodeKeys',
+			[ 'recoverycodekeys' => $recCodeKeys ]
+		);
+		$recCodeKeysForContent = $this->getRecoveryCodesForDisplay( $recCodeKeysForDisplay );
+		$this->getOutput()->addModules( 'ext.oath.recovery' );
+		$this->getOutput()->addModuleStyles( 'ext.oath.recovery.styles' );
+		$this->setOutputJsConfigVars( $recCodeKeysForContent );
 
-		return $html;
+		return $html . $this->generateRecoveryCodesContent( $recCodeKeysForContent, true );
 	}
 
 	/**
@@ -103,27 +100,27 @@ class WebAuthnAddKeyForm extends OATHAuthOOUIHTMLForm {
 		$registrationResult = $authenticator->continueRegistration( $credential );
 		if ( $registrationResult->isGood() ) {
 
-			if ( $this->getConfig()->get( 'OATHAllowMultipleModules' ) ) {
-				// handle new recovery codes
-				$moduleDbKeys = $this->oathUser->getKeysForModule( RecoveryCodes::MODULE_NAME );
+			// handle new recovery codes
+			$moduleDbKeys = $this->oathUser->getKeysForModule( RecoveryCodes::MODULE_NAME );
 
-				// only create recovery code module entry if this is the first 2fa key a user is creating
-				if ( count( $moduleDbKeys ) > RecoveryCodeKeys::RECOVERY_CODE_MODULE_COUNT ) {
-					throw new UnexpectedValueException(
-						$this->msg( 'oathauth-recoverycodes-too-many-instances' )->escaped()
-					);
-				} elseif ( count( $moduleDbKeys ) < RecoveryCodeKeys::RECOVERY_CODE_MODULE_COUNT ) {
-					$keyData = $this->getKeyDataInSession( 'RecoveryCodeKeys' );
-					$recCodeKeys = RecoveryCodeKeys::newFromArray( $keyData );
-					$this->setKeyDataInSessionToNull( 'RecoveryCodeKeys' );
-					$moduleRegistry = OATHAuthServices::getInstance()->getModuleRegistry();
-					$this->oathRepo->createKey(
-						$this->oathUser,
-						$moduleRegistry->getModuleByKey( RecoveryCodes::MODULE_NAME ),
-						$recCodeKeys->jsonSerialize(),
-						$this->getRequest()->getIP()
-					);
-				}
+			// only create a recovery code module entry if this is the first 2FA key a user is creating
+			if ( count( $moduleDbKeys ) > RecoveryCodeKeys::RECOVERY_CODE_MODULE_COUNT ) {
+				throw new UnexpectedValueException(
+					$this->msg( 'oathauth-recoverycodes-too-many-instances' )->escaped()
+				);
+			}
+
+			if ( count( $moduleDbKeys ) < RecoveryCodeKeys::RECOVERY_CODE_MODULE_COUNT ) {
+				$keyData = $this->getKeyDataInSession( 'RecoveryCodeKeys' );
+				$recCodeKeys = RecoveryCodeKeys::newFromArray( $keyData );
+				$this->setKeyDataInSessionToNull( 'RecoveryCodeKeys' );
+				$moduleRegistry = OATHAuthServices::getInstance()->getModuleRegistry();
+				$this->oathRepo->createKey(
+					$this->oathUser,
+					$moduleRegistry->getModuleByKey( RecoveryCodes::MODULE_NAME ),
+					$recCodeKeys->jsonSerialize(),
+					$this->getRequest()->getIP()
+				);
 			}
 
 			return true;
