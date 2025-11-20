85e8a269784ca620d7cddc9bdf85dd917630580d
Sam Reed <sam@reedyboy.net>
2025-11-20 21:49:16 +0000

    WebAuthnKeyTest: Reduce data duplication and use assertTrue/assertFalse
    Follows-Up: I9ff9d4dc855c7dfbf251b83b52fda5ca29491230
    Change-Id: Ie81a598f0cc7169c63d21a64abbdc05f3af9ca0a

--- tests/phpunit/integration/WebAuthnKeyTest.php
+++ tests/phpunit/integration/WebAuthnKeyTest.php
@@ -10,29 +10,34 @@ use MediaWikiIntegrationTestCase;
  */
 class WebAuthnKeyTest extends MediaWikiIntegrationTestCase {
 
+	private const KEY_DATA = [
+		'userHandle' => 'fakeHandle',
+		'friendlyName' => 'testKey',
+		'counter' => 3,
+		'transports' => [],
+		'aaguid' => 'f7dcf1ec-76ad-49cb-ae2a-6d8ed6736f88',
+		'publicKeyCredentialId' => 'none',
+		'credentialPublicKey' => 'none',
+	];
+
 	public function testPasswordlessLoginPasskeysSupportsPasswordless(): void {
 		$key = WebAuthnKey::newFromData(
-		[ 'supportsPasswordless' => true, 'userHandle' => 'fakeHandle',
-				'friendlyName' => 'testKey', 'counter' => 3, 'transports' => [],
-				'aaguid' => 'f7dcf1ec-76ad-49cb-ae2a-6d8ed6736f88', 'publicKeyCredentialId' => 'none',
-				'credentialPublicKey' => 'none' ] );
-		$this->assertSame( true, $key->supportsPasswordlessLogin() );
+			self::KEY_DATA + [ 'supportsPasswordless' => true ]
+		);
+		$this->assertTrue( $key->supportsPasswordlessLogin() );
 	}
 
 	public function testPasswordlessLoginPasskeysDoesntSupportPasswordless(): void {
 		$key = WebAuthnKey::newFromData(
-		[ 'supportsPasswordless' => false, 'userHandle' => 'fakeHandle',
-				'friendlyName' => 'testKey', 'counter' => 3, 'transports' => [],
-				'aaguid' => 'f7dcf1ec-76ad-49cb-ae2a-6d8ed6736f88', 'publicKeyCredentialId' => 'none',
-				'credentialPublicKey' => 'none' ] );
-		$this->assertSame( false, $key->supportsPasswordlessLogin() );
+			self::KEY_DATA + [ 'supportsPasswordless' => false ]
+		);
+		$this->assertFalse( $key->supportsPasswordlessLogin() );
 	}
 
 	public function testPasswordlessLoginPasskeysSupportPasswordlessMissing(): void {
 		$key = WebAuthnKey::newFromData(
-		[ 'friendlyName' => 'testKey', 'userHandle' => 'fakeHandle', 'counter' => 3, 'transports' => [],
-				'aaguid' => 'f7dcf1ec-76ad-49cb-ae2a-6d8ed6736f88', 'publicKeyCredentialId' => 'none',
-				'credentialPublicKey' => 'none' ] );
-		$this->assertSame( false, $key->supportsPasswordlessLogin() );
+			self::KEY_DATA
+		);
+		$this->assertFalse( $key->supportsPasswordlessLogin() );
 	}
 }
