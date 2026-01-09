606394604e23557a3cd0670adaccf3f32c68fba5
Antoine Musso <hashar@free.fr>
2026-01-09 15:56:10 +0100

    tests: skip test when WebAuthn is not loaded
    testPasskeysSectionAllowsAddingPasskeysWhenUserHas2fa would fail when
    WebAuthn is not loaded: oathauth-passkeys-header is not found in the
    output.
    
    Disable the test when it is not loaded. On CI OATHAuth has WebAuthn
    marked as a dependency and the test will run.
    
    Bug: T407797
    Change-Id: Idb15bc78429d82de59df820ce398d7845db0c2ce
    (cherry picked from commit 192e781605a046d760a96ccc9b7b229ce3487535)

--- tests/phpunit/integration/Special/OATHManageTest.php
+++ tests/phpunit/integration/Special/OATHManageTest.php
@@ -144,6 +144,8 @@ class OATHManageTest extends SpecialPageTestBase {
 	}
 
 	public function testPasskeysSectionAllowsAddingPasskeysWhenUserHas2fa() {
+		$this->markTestSkippedIfExtensionNotLoaded( 'WebAuthn' );
+
 		// Setup user + existing TOTP key
 		$user = $this->getTestUser()->getUser();
 		$userRepo = OATHAuthServices::getInstance( $this->getServiceContainer() )->getUserRepository();
