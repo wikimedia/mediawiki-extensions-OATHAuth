2c1287158584ea5f56f9a0e77d1610f380ee655b
Marcin Szwarc <mszwarc@wikimedia.org>
2026-02-05 13:30:18 +0100

    Recover2FAForUser: Send additional recovery codes by email
    Why:
    - We want the recovery codes to be delivered to user by email,
      eliminating need for T&S to manually copy-and-paste them.
      - This also reduces their exposure, by not having a human proxy who
        can see the codes.
    
    What:
    - Upon generation of the codes, send them by email, instead of
      displaying on the success screen.
    - Add a field to the form, where recipient's email can be specified
      (if target user has no e-mail set).
    
    Bug: T415883
    Change-Id: Ifdfe8f9b98eef34a15f7046412aa930f20ba8abc

--- src/Special/Recover2FAForUser.php
+++ src/Special/Recover2FAForUser.php
@@ -6,21 +6,25 @@ use MediaWiki\Extension\OATHAuth\Module\RecoveryCodes;
 use MediaWiki\Extension\OATHAuth\OATHAuthLogger;
 use MediaWiki\Extension\OATHAuth\OATHAuthModuleRegistry;
 use MediaWiki\Extension\OATHAuth\OATHUserRepository;
-use MediaWiki\Html\Html;
 use MediaWiki\HTMLForm\HTMLForm;
 use MediaWiki\Linker\LinkRenderer;
+use MediaWiki\Mail\IEmailer;
+use MediaWiki\Mail\MailAddress;
+use MediaWiki\MainConfigNames;
+use MediaWiki\Parser\Sanitizer;
+use MediaWiki\Registration\ExtensionRegistry;
 use MediaWiki\SpecialPage\FormSpecialPage;
+use MediaWiki\Status\Status;
 use MediaWiki\User\CentralId\CentralIdLookup;
+use MediaWiki\User\Options\UserOptionsLookup;
 use MediaWiki\User\User;
 use MediaWiki\User\UserFactory;
 use MediaWiki\User\UserIdentity;
-use UnexpectedValueException;
+use Wikimedia\Timestamp\ConvertibleTimestamp;
 
 class Recover2FAForUser extends FormSpecialPage {
 
 	private readonly int $codesCount;
-	/** @var list<string>|null New recovery codes will be stored here after successful generation */
-	private ?array $newRecoveryCodes = null;
 	private ?UserIdentity $targetUser = null;
 
 	public function __construct(
@@ -30,6 +34,9 @@ class Recover2FAForUser extends FormSpecialPage {
 		private readonly UserFactory $userFactory,
 		private readonly CentralIdLookup $centralIdLookup,
 		private readonly LinkRenderer $linkRenderer,
+		private readonly ExtensionRegistry $extensionRegistry,
+		private readonly IEmailer $emailer,
+		private readonly UserOptionsLookup $userOptionsLookup,
 	) {
 		// messages used: recover2faforuser (display "name" on Special:SpecialPages),
 		// right-oathauth-recover-for-user, action-oathauth-recover-for-user
@@ -89,6 +96,10 @@ class Recover2FAForUser extends FormSpecialPage {
 
 	/** @inheritDoc */
 	protected function getFormFields() {
+		$user = $this->getUserByName( $this->getRequest()->getText( 'user' ) );
+		$oathUser = $user ? $this->userRepo->findByUser( $user ) : null;
+		$showEmailField = $user && $this->getUserEmail( $user ) === null && $oathUser?->isTwoFactorAuthEnabled();
+
 		return [
 			'user' => [
 				'type' => 'user',
@@ -105,23 +116,37 @@ class Recover2FAForUser extends FormSpecialPage {
 				'name' => 'reason',
 				'required' => true,
 			],
+			'email' => [
+				'type' => $showEmailField ? 'email' : 'hidden',
+				'default' => '',
+				'label-message' => 'oathauth-enterrecoveremail',
+				'help-message' => 'oathauth-enterrecoveremail-help',
+				'name' => 'email',
+				'required' => false,
+			],
 		];
 	}
 
 	/** @inheritDoc */
-	public function onSubmit( array $formData ) {
-		$user = $this->userFactory->newFromName( $formData['user'] );
-		// T393253 - Check the username is valid, but don't check if it exists on the local wiki.
-		// Instead, check there is a valid central ID.
-		if ( !$user || $this->centralIdLookup->centralIdFromName( $formData['user'] ) === 0 ) {
-			return [ 'oathauth-user-not-found' ];
+	public function onSubmit( array $formData ): Status {
+		$user = $this->getUserByName( $formData['user'] );
+		if ( !$user ) {
+			return Status::newFatal( 'oathauth-user-not-found' );
 		}
 		$this->targetUser = $user;
 
 		$oathUser = $this->userRepo->findByUser( $user );
-
 		if ( !$oathUser->isTwoFactorAuthEnabled() ) {
-			return [ 'oathauth-recover-fail-no-2fa' ];
+			return Status::newFatal( 'oathauth-recover-fail-no-2fa' );
+		}
+
+		$userEmail = $this->getUserEmail( $user );
+		if ( $userEmail === null ) {
+			// Attempt to use the one provided in the form
+			$userEmail = $formData['email'];
+			if ( !Sanitizer::validateEmail( $userEmail ) ) {
+				return Status::newFatal( 'oathauth-recover-fail-email-required' );
+			}
 		}
 
 		/** @var RecoveryCodes $recoveryCodesModule */
@@ -129,33 +154,100 @@ class Recover2FAForUser extends FormSpecialPage {
 		'@phan-var RecoveryCodes $recoveryCodesModule';
 
 		$key = $recoveryCodesModule->ensureExistence( $oathUser );
-		$this->newRecoveryCodes = $key->generateAdditionalRecoveryCodeKeys( $this->codesCount );
+		$newRecoveryCodes = $key->generateAdditionalRecoveryCodeKeys( $this->codesCount );
 		$this->userRepo->updateKey( $oathUser, $key );
 
-		$this->oathLogger->logOATHRecovery( $this->getUser(), $user, $formData['reason'], $this->codesCount );
+		$emailStatus = $this->sendEmailWithRecoveryCodes( $userEmail, $newRecoveryCodes, $user );
+		if ( !$emailStatus->isOK() ) {
+			return $emailStatus;
+		}
 
-		return true;
+		$this->oathLogger->logOATHRecovery( $this->getUser(), $user, $formData['reason'], $this->codesCount );
+		return Status::newGood();
 	}
 
 	public function onSuccess() {
-		if ( $this->newRecoveryCodes === null ) {
-			throw new UnexpectedValueException( 'New recovery codes have not been generated' );
-		}
-
 		$targetUserName = $this->targetUser->getName();
 		$targetUserLink = $this->linkRenderer->makeUserLink( $this->targetUser, $this->getContext() );
 
-		$codesListItems = '';
-		foreach ( $this->newRecoveryCodes as $code ) {
-			$codesListItems .= Html::rawElement( 'li', [], Html::element( 'code', [], $code ) );
-		}
-		$codesList = Html::rawElement( 'ul', [], $codesListItems );
-
 		$successMsg = $this->msg( 'oathauth-recoveredoath' )
-			->params( count( $this->newRecoveryCodes ), $targetUserName )
+			->params( $this->codesCount, $targetUserName )
 			->rawParams( $targetUserLink );
 		$this->getOutput()->addWikiMsg( $successMsg );
-		$this->getOutput()->addHTML( $codesList );
 		$this->getOutput()->returnToMain();
+	}
+
+	private function getUserEmail( User $user ): ?string {
+		$email = $user->getEmail();
+		if ( $email !== '' ) {
+			return $email;
+		}
+		return null;
+	}
+
+	private function getUserByName( string $username ): ?User {
+		$user = $this->userFactory->newFromName( $username );
+		// T393253 - Check the username is valid, but don't check if it exists on the local wiki.
+		// Instead, check there is a valid central ID.
+		if ( !$user || $this->centralIdLookup->centralIdFromName( $username ) === 0 ) {
+			return null;
+		}
+		return $user;
+	}
+
+	private function sendEmailWithRecoveryCodes(
+		string $emailAddress,
+		array $recoveryCodes,
+		User $targetUser
+	): Status {
+		// PasswordSender is used across MediaWiki for different purposes, that's why we use it here as well.
+		$passwordSender = $this->getConfig()->get( MainConfigNames::PasswordSender );
+		$sender = new MailAddress(
+			$passwordSender,
+			$this->msg( 'emailsender' )->inContentLanguage()->text()
+		);
+		$to = new MailAddress( $emailAddress );
+
+		$recoveryCodesText = implode( "\n", $recoveryCodes );
+
+		if ( $targetUser->isRegistered() ) {
+			$userLanguage = $this->userOptionsLookup->getOption( $targetUser, 'language' );
+		} else {
+			$userLanguage = $this->getContext()->getLanguage()->getCode();
+		}
+
+		$siteAdminContact = trim(
+			$this->msg( 'oathauth-recover-email-text-site-admin-contact' )
+				->inContentLanguage()
+				->text()
+		);
+		if ( $siteAdminContact ) {
+			$siteAdminContact = '<' . $siteAdminContact . '>';
+			$contactAdminLine = $this->msg( 'oathauth-recover-email-text-please-contact-with-address' )
+				->inLanguage( $userLanguage )
+				->params( $targetUser->getName(), $siteAdminContact )
+				->text();
+		} else {
+			$contactAdminLine = $this->msg( 'oathauth-recover-email-text-please-contact' )
+				->inLanguage( $userLanguage )
+				->params( $targetUser->getName() )
+				->text();
+		}
+
+		$now = ConvertibleTimestamp::now();
+
+		$subject = $this->msg( 'oathauth-recover-email-title' )
+			->inLanguage( $userLanguage )
+			->params( count( $recoveryCodes ) )
+			->text();
+		$body = $this->msg( 'oathauth-recover-email-text' )
+			->inLanguage( $userLanguage )
+			->params( $targetUser->getName(), count( $recoveryCodes ), $recoveryCodesText, $contactAdminLine )
+			->dateTimeParams( $now )
+			->dateParams( $now )
+			->timeParams( $now )
+			->text();
+
+		return Status::wrap( $this->emailer->send( $to, $sender, $subject, $body ) );
 	}
 }
--- tests/phpunit/integration/Special/Recover2FAForUserTest.php
+++ tests/phpunit/integration/Special/Recover2FAForUserTest.php
@@ -14,10 +14,12 @@ use MediaWiki\Extension\OATHAuth\Module\RecoveryCodes;
 use MediaWiki\Extension\OATHAuth\OATHAuthLogger;
 use MediaWiki\Extension\OATHAuth\OATHAuthServices;
 use MediaWiki\Extension\OATHAuth\Special\Recover2FAForUser;
+use MediaWiki\Mail\IEmailer;
 use MediaWiki\MainConfigNames;
 use MediaWiki\Registration\ExtensionRegistry;
 use MediaWiki\Request\FauxRequest;
 use SpecialPageTestBase;
+use StatusValue;
 
 /**
  * @group Database
@@ -46,6 +48,9 @@ class Recover2FAForUserTest extends SpecialPageTestBase {
 			$this->getServiceContainer()->getUserFactory(),
 			$this->getServiceContainer()->getCentralIdLookup(),
 			$this->getServiceContainer()->getLinkRenderer(),
+			$this->getServiceContainer()->getExtensionRegistry(),
+			$this->getServiceContainer()->getEmailer(),
+			$this->getServiceContainer()->getUserOptionsLookup(),
 		);
 	}
 
@@ -118,12 +123,28 @@ class Recover2FAForUserTest extends SpecialPageTestBase {
 		$this->assertStringContainsString( '(oathauth-recover-fail-no-2fa)', $html );
 	}
 
-	public function testGeneratedAdditionalCodes() {
+	/** @dataProvider provideGeneratedAdditionalCodes */
+	public function testGeneratedAdditionalCodes(
+		?string $userEmail,
+		?string $enteredEmail,
+		string $expectedEmail
+	) {
 		$loggerMock = $this->createMock( OATHAuthLogger::class );
 		$loggerMock->expects( $this->once() )->method( 'logOATHRecovery' );
 		$this->setService( 'OATHAuthLogger', $loggerMock );
 
-		$otherUser = $this->getTestUser()->getUser();
+		$otherUser = $this->getMutableTestUser()->getUser();
+		$otherUser->setEmail( $userEmail ?? '' );
+		$otherUser->saveSettings();
+
+		$mailerMock = $this->createMock( IEmailer::class );
+		$mailerMock->expects( $this->once() )
+			->method( 'send' )
+			->willReturnCallback( function ( $to ) use ( $expectedEmail ) {
+				$this->assertSame( $expectedEmail, $to->address );
+				return StatusValue::newGood();
+			} );
+		$this->setService( 'Emailer', $mailerMock );
 
 		$userRepo = OATHAuthServices::getInstance( $this->getServiceContainer() )->getUserRepository();
 		$moduleRegistry = OATHAuthServices::getInstance( $this->getServiceContainer() )->getModuleRegistry();
@@ -144,15 +165,17 @@ class Recover2FAForUserTest extends SpecialPageTestBase {
 
 		$user = $this->getTestSysop()->getUser();
 		RequestContext::getMain()->getRequest()->getSession()->setUser( $user );
+
+		$requestData = [
+			'reason' => $reason,
+			'user' => $otherUser->getName(),
+		];
+		if ( $enteredEmail !== null ) {
+			$requestData['email'] = $enteredEmail;
+		}
 		[ $html ] = $this->executeSpecialPage(
 			'',
-			new FauxRequest(
-				[
-					'reason' => $reason,
-					'user' => $otherUser->getName(),
-				],
-				true,
-			),
+			new FauxRequest( $requestData, true ),
 			null,
 			$user,
 		);
@@ -167,5 +190,77 @@ class Recover2FAForUserTest extends SpecialPageTestBase {
 		$this->assertCount( 12, $newCodes );
 
 		$this->assertSame( [ 'H8572S2FB1LCGYWN', 'V61A5VEM42DGLDMU' ], array_slice( $newCodes, 0, 2 ) );
+	}
+
+	public static function provideGeneratedAdditionalCodes(): array {
+		return [
+			'User has email, no email entered' => [
+				'userEmail' => 'user@example.com',
+				'enteredEmail' => null,
+				'expectedEmail' => 'user@example.com',
+			],
+			// Ensure that user's own email takes precedence
+			'User has email, email entered' => [
+				'userEmail' => 'user@example.com',
+				'enteredEmail' => 'other@example.com',
+				'expectedEmail' => 'user@example.com',
+			],
+			'User has no email, email entered' => [
+				'userEmail' => null,
+				'enteredEmail' => 'other@example.com',
+				'expectedEmail' => 'other@example.com',
+			],
+		];
+	}
+
+	public function testAsksForEmailIfUserHasNoEmail() {
+		// This user has no e-mail address
+		$otherUser = $this->getMutableTestUser()->getUser();
+		$otherUser->setEmail( '' );
+		$otherUser->saveSettings();
+
+		$userRepo = OATHAuthServices::getInstance( $this->getServiceContainer() )->getUserRepository();
+		$moduleRegistry = OATHAuthServices::getInstance( $this->getServiceContainer() )->getModuleRegistry();
+		$recoveryCodesModule = $moduleRegistry->getModuleByKey( RecoveryCodes::MODULE_NAME );
+
+		$oathUser = $userRepo->findByUser( $otherUser );
+		$userRepo->createKey( $oathUser, $recoveryCodesModule, [
+			'recoverycodekeys' => [ 'H8572S2FB1LCGYWN', 'V61A5VEM42DGLDMU' ],
+		], '127.0.0.1' );
+
+		$keys = $oathUser->getKeysForModule( RecoveryCodes::MODULE_NAME );
+		$this->assertCount( 1, $keys );
+		$key = $keys[0];
+		$this->assertInstanceOf( RecoveryCodeKeys::class, $key );
+		$this->assertCount( 2, $key->getRecoveryCodeKeys() );
+
+		$reason = 'I am required!';
+
+		$user = $this->getTestSysop()->getUser();
+		RequestContext::getMain()->getRequest()->getSession()->setUser( $user );
+
+		[ $html ] = $this->executeSpecialPage(
+			'',
+			new FauxRequest(
+				[
+					'reason' => $reason,
+					'user' => $otherUser->getName(),
+				],
+				true
+			),
+			null,
+			$user,
+		);
+
+		$this->assertStringContainsString( '(oathauth-recover-fail-email-required)', $html );
+		$this->assertStringContainsString( '(oathauth-enterrecoveremail)', $html );
+
+		// Ensure that no codes were generated
+		$keys = $oathUser->getKeysForModule( RecoveryCodes::MODULE_NAME );
+		$this->assertCount( 1, $keys );
+		$newKey = $keys[0];
+		$this->assertInstanceOf( RecoveryCodeKeys::class, $newKey );
+		$newCodes = $newKey->getRecoveryCodeKeys();
+		$this->assertCount( 2, $newCodes );
 	}
 }
