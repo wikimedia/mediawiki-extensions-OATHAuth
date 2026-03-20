4029149e4ed109c8c7465f5021357baef345268b
Sam Reed <sam@reedyboy.net>
2026-03-20 21:52:51 +0000

    tests: Add test for ReEncryptSecrets maintenance script
    Bug: T404817
    Change-Id: Ia3df73245d1709d034fd30ba84db360cf2e7976f

---
+++ tests/phpunit/integration/Maintenance/ReEncryptSecretsTest.php
@@ -0,0 +1,138 @@
+<?php
+declare( strict_types=1 );
+
+namespace MediaWiki\Extension\OATHAuth\Tests\Integration;
+
+use MediaWiki\Extension\OATHAuth\Key\RecoveryCode;
+use MediaWiki\Extension\OATHAuth\Key\RecoveryCodeKeys;
+use MediaWiki\Extension\OATHAuth\Key\TOTPKey;
+use MediaWiki\Extension\OATHAuth\Maintenance\ReEncryptSecrets;
+use MediaWiki\Extension\OATHAuth\Module\RecoveryCodes;
+use MediaWiki\Extension\OATHAuth\Module\TOTP;
+use MediaWiki\Extension\OATHAuth\OATHAuthServices;
+use MediaWiki\Extension\OATHAuth\OATHUserRepository;
+use MediaWiki\MainConfigNames;
+use MediaWiki\Tests\Maintenance\MaintenanceBaseTestCase;
+use MediaWiki\User\User;
+
+/**
+ * @covers \MediaWiki\Extension\OATHAuth\Maintenance\ReEncryptSecrets
+ * @group Database
+ */
+class ReEncryptSecretsTest extends MaintenanceBaseTestCase {
+
+	use EncryptionTestTrait;
+
+	private const string NEW_SECRET_KEY = '22e715e0c785dde0d0f39f8ed94e9f2419ad8d01e91e9343902afba8aaa4ec91';
+
+	protected function setUp(): void {
+		parent::setUp();
+
+		$this->encryptionIntegrationTestSetup();
+	}
+
+	protected function getMaintenanceClass() {
+		return ReEncryptSecrets::class;
+	}
+
+	public function testMismatchInputVersusConfig() {
+		$this->maintenance->setArg( 0, 'foo' );
+		$this->maintenance->setArg( 1, 'bar' );
+
+		$this->expectCallToFatalError();
+		$this->expectOutputString(
+			"The old key is not the same as \$wgOATHSecretKey. Unable to decrypt existing secrets."
+		);
+
+		$this->maintenance->execute();
+	}
+
+	public function testIdenticalInput() {
+		$this->maintenance->setArg( 0, 'foo' );
+		$this->maintenance->setArg( 1, 'foo' );
+
+		$this->expectCallToFatalError();
+		$this->expectOutputString(
+			"The old key is the same as the new key. No reason to re-encrypt existing secrets."
+		);
+
+		$this->maintenance->execute();
+	}
+
+	public function testInvalidFirstSecret() {
+		$this->maintenance->setArg( 0, 'foo' );
+		$this->maintenance->setArg( 1, self::NEW_SECRET_KEY );
+
+		$this->expectCallToFatalError();
+		$this->expectOutputString( "The 'old' parameter is not set correctly!" );
+
+		$this->maintenance->execute();
+	}
+
+	public function testInvalidSecondSecret() {
+		$this->maintenance->setArg( 0, self::SECRET_KEY );
+		$this->maintenance->setArg( 1, 'foo' );
+
+		$this->expectCallToFatalError();
+		$this->expectOutputString( "The 'new' parameter is not set correctly!" );
+
+		$this->maintenance->execute();
+	}
+
+	private function setupUserWith2FA(): array {
+		// Ensure to use local because CentralAuth may exist in CI
+		$this->overrideConfigValues( [
+			MainConfigNames::CentralIdLookupProvider => 'local',
+		] );
+
+		$user = $this->getTestSysop()->getUser();
+		$services = OATHAuthServices::getInstance( $this->getServiceContainer() );
+		$repository = $services->getUserRepository();
+		$moduleRegistry = $services->getModuleRegistry();
+
+		$oathUser = $repository->findByUser( $user );
+
+		$repository->createKey(
+			$oathUser,
+			$moduleRegistry->getModuleByKey( TOTP::MODULE_NAME ),
+			TOTPKey::newFromRandom()->jsonSerialize(),
+			'127.0.0.1'
+		);
+
+		$recoveryCodes = new RecoveryCodeKeys( null, null, null, [] );
+		$recoveryCodes->regenerateRecoveryCodeKeys();
+
+		$repository->createKey(
+			$oathUser,
+			$moduleRegistry->getModuleByKey( RecoveryCodes::MODULE_NAME ),
+			$recoveryCodes->jsonSerialize(),
+			'127.0.0.1'
+		);
+
+		return [ $repository, $user, $recoveryCodes->getRecoveryCodeKeys() ];
+	}
+
+	public function testReEncyptSecrets() {
+		/** @var OATHUserRepository $repository */
+		/** @var User $user */
+		/** @var RecoveryCode[] $recoveryKeys */
+		[ $repository, $user, $recoveryKeys ] = $this->setupUserWith2FA();
+
+		$this->maintenance->setArg( 0, self::SECRET_KEY );
+		$this->maintenance->setArg( 1, self::NEW_SECRET_KEY );
+		$this->maintenance->execute();
+
+		$this->expectOutputString(
+			"Please set \$wgOATHSecretKey to the new value in LocalSettings.php.\n"
+		);
+
+		$this->overrideConfigValue( 'OATHSecretKey', self::NEW_SECRET_KEY );
+		$this->getServiceContainer()->resetServiceForTesting( 'OATHAuth.EncryptionHelper' );
+
+		$oathUser = $repository->findByUser( $user );
+		$modules = $oathUser->getKeysForModule( RecoveryCodes::MODULE_NAME );
+		$this->assertCount( 1, $modules );
+
+		$this->assertTrue( $modules[0]->verify( $oathUser, [ 'recoverycode' => $recoveryKeys[0] ] ) );
+	}
+}
