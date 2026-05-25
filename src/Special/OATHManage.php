ce0e8434d16a3a29773a5e764c5b3485c9159eca
Marcin Szwarc <mszwarc@wikimedia.org>
2026-05-25 09:59:26 +0200

    Allow to remove passkeys when there's only one standard 2FA method
    Why:
    - When removing a 2FA method, if user is required to have 2FA on, we
      count all non-special and non-passwordless methods, and if the
      result is 1, we pevent from disabling 2FA methods.
    - This check doesn't account for the fact that disabling
      passwordless methods should be still permitted (they don't count
      towards the limit but are "extra", so removing the will not cause
      the user to drop below the limit).
    
    What:
    - Adjust the logic for displaying a warning in place of method removal
      form, so that we allow to remove a passwordless method when there's
      only one "normal" one.
    
    Bug: T426872
    Change-Id: Ia4a594263f53ecc8d6ba55e0265df1506682fc08

--- src/Special/OATHManage.php
+++ src/Special/OATHManage.php
@@ -781,7 +781,7 @@ class OATHManage extends SpecialPage {
 			);
 		}
 
-		if ( !$this->canRemoveKeys() ) {
+		if ( !$keyToDelete->supportsPasswordlessLogin() && !$this->canRemoveKeys() ) {
 			throw new ErrorPageError(
 				'oathauth-disable',
 				'oathauth-remove-lastkey-required'
