e210d93a1b9b24e23023d60c05e362099d5e5e87
Sam Reed <sam@reedyboy.net>
2026-05-28 16:00:37 +0100

    tests: Add another test case for PopulateUserHandles
    Bug: T404817
    Change-Id: I93b72d8c65f84daa5673826af42a5ba0ed4fda58

--- tests/phpunit/integration/Maintenance/PopulateUserHandlesTest.php
+++ tests/phpunit/integration/Maintenance/PopulateUserHandlesTest.php
@@ -3,7 +3,10 @@ declare( strict_types=1 );
 
 namespace MediaWiki\Extension\OATHAuth\Tests\Integration\Maintenance;
 
+use MediaWiki\Extension\OATHAuth\Key\WebAuthnKey;
 use MediaWiki\Extension\OATHAuth\Maintenance\PopulateUserHandles;
+use MediaWiki\Extension\OATHAuth\Module\WebAuthn;
+use MediaWiki\Extension\OATHAuth\Tests\Integration\Key\WebAuthnKeyTest as KeyIntegrationTest;
 use MediaWiki\Tests\Maintenance\MaintenanceBaseTestCase;
 
 /**
@@ -11,6 +14,8 @@ use MediaWiki\Tests\Maintenance\MaintenanceBaseTestCase;
  * @group Database
  */
 class PopulateUserHandlesTest extends MaintenanceBaseTestCase {
+
+	use UserWith2FATrait;
 
 	protected function getMaintenanceClass() {
 		return PopulateUserHandles::class;
@@ -21,6 +26,24 @@ class PopulateUserHandlesTest extends MaintenanceBaseTestCase {
 			"Done. Processed 0 users and inserted 0 rows"
 		);
 
+		$this->maintenance->execute();
+	}
+
+	public function testPopulateUserWith2FA(): void {
+		[ $repository, $moduleRegistry, $oathUser, ] = $this->setupConfig();
+
+		$key = WebAuthnKey::newFromData(
+			KeyIntegrationTest::KEY_DATA
+		);
+
+		$repository->createKey(
+			$oathUser,
+			$moduleRegistry->getModuleByKey( WebAuthn::MODULE_NAME ),
+			$key->jsonSerialize(),
+			'127.0.0.1'
+		);
+
+		$this->expectOutputString( "Done. Processed 1 users and inserted 1 rows" );
 		$this->maintenance->execute();
 	}
 }
