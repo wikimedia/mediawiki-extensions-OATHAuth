bcfd6f0017ba96c3fe0a416a20a1df60b6d18ad4
Sam Reed <sam@reedyboy.net>
2026-06-01 23:49:09 +0100

    Recover2FAForUserTest: Add coverage for email sent
    Bug: T404817
    Change-Id: If66f4970c34d7653071582ef935d91fda108a515

--- tests/phpunit/integration/Maintenance/Recover2FAForUserTest.php
+++ tests/phpunit/integration/Maintenance/Recover2FAForUserTest.php
@@ -4,7 +4,9 @@ declare( strict_types=1 );
 namespace MediaWiki\Extension\OATHAuth\Tests\Integration\Maintenance;
 
 use MediaWiki\Extension\OATHAuth\Maintenance\Recover2FAForUser;
+use MediaWiki\Mail\IEmailer;
 use MediaWiki\Tests\Maintenance\MaintenanceBaseTestCase;
+use StatusValue;
 
 /**
  * @covers \MediaWiki\Extension\OATHAuth\Maintenance\Recover2FAForUser
@@ -12,14 +14,39 @@ use MediaWiki\Tests\Maintenance\MaintenanceBaseTestCase;
  */
 class Recover2FAForUserTest extends MaintenanceBaseTestCase {
 
+	use UserWith2FATrait;
+
+	private const string EMAIL_ADDRESS = 'foobar@email.com';
+
 	protected function getMaintenanceClass() {
 		return Recover2FAForUser::class;
 	}
 
 	public function testNonExistentUser(): void {
 		$this->maintenance->setArg( 'user', 'foobar' );
-		$this->maintenance->setArg( 'email', 'foobar@email.com' );
+		$this->maintenance->setArg( 'email', self::EMAIL_ADDRESS );
 		$this->expectOutputString( "User foobar doesn't exist!" );
+		$this->maintenance->execute();
+	}
+
+	public function testRecover2FAForUser(): void {
+		[ , $user, , , ] = $this->setupUserWith2FA();
+
+		$user->setEmail( self::EMAIL_ADDRESS );
+
+		$mailerMock = $this->createMock( IEmailer::class );
+		$mailerMock->expects( $this->once() )
+			->method( 'send' )
+			->willReturnCallback( function ( $to ) {
+				$this->assertSame( self::EMAIL_ADDRESS, $to->address );
+				return StatusValue::newGood();
+			} );
+		$this->setService( 'Emailer', $mailerMock );
+
+		$username = $user->getName();
+		$this->maintenance->setArg( 'user', $username );
+		$this->maintenance->setArg( 'email', self::EMAIL_ADDRESS );
+		$this->expectOutputString( 'Expiring recovery codes generated successfully and emailed to ' . $username );
 		$this->maintenance->execute();
 	}
 }
