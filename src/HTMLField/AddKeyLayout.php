070c591572efef51c4d8a79be1ef1da9c8311b78
Derk-Jan Hartman <hartman.wiki@gmail.com>
2026-06-26 00:27:43 +0200

    a11y: Implement getOOUI of AddKeyLayout form
    Previously getOOUI fell back to wrapping getInputHTML output in an opaque
    Widget. The outer FieldLayout could not resolve an inputId, so the label
    was not connected to the actual <input> element, which is an
    accessibility problem.
    
    Override getOOUI to return ActionFieldLayout directly as a proper
    custom OOUI htmlform widget.
    
    Bug: T406842
    Change-Id: I7939318d1d221c9fae11929fc852db46390c33e2

--- src/HTMLField/AddKeyLayout.php
+++ src/HTMLField/AddKeyLayout.php
@@ -6,32 +6,44 @@ namespace MediaWiki\Extension\OATHAuth\HTMLField;
 use MediaWiki\HTMLForm\HTMLFormField;
 use OOUI\ActionFieldLayout;
 use OOUI\ButtonWidget;
+use OOUI\HtmlSnippet;
 use OOUI\TextInputWidget;
 
 class AddKeyLayout extends HTMLFormField {
 
-	/**
-	 * @param string $value
-	 * @return string The ActionFieldLayout, stringified
-	 */
+	/** @inheritDoc */
 	public function getInputHTML( $value ) {
-		// We initiate the fields disabled, to avoid user interacting
-		// with the form, until the client-side script is loaded and ready
-		$input = new TextInputWidget( [
+		return (string)new ActionFieldLayout( $this->makeInputWidget(), $this->makeButtonWidget() );
+	}
+
+	/** @inheritDoc */
+	public function getOOUI( $value ) {
+		$label = $this->getLabel();
+		return new ActionFieldLayout( $this->makeInputWidget(), $this->makeButtonWidget(), [
+			'label' => $label !== '' ? new HtmlSnippet( $label ) : null,
+			'align' => 'top',
+		] );
+	}
+
+	private function makeInputWidget(): TextInputWidget {
+		return new TextInputWidget( [
 			'id' => 'key_name',
 			'name' => 'key_name',
 			'required' => false,
 			'infusable' => true,
-			'disabled' => true,
 			'autofocus' => true,
 		] );
-		$button = new ButtonWidget( [
+	}
+
+	private function makeButtonWidget(): ButtonWidget {
+		// We initiate the button disabled, to avoid user interacting
+		// with the form, until the client-side script is loaded and ready
+		return new ButtonWidget( [
 			'flags' => [ 'primary', 'progressive' ],
 			'label' => wfMessage( 'oathauth-webauthn-ui-add-key' )->plain(),
 			'disabled' => true,
 			'id' => 'button_add_key',
-			'infusable' => true
+			'infusable' => true,
 		] );
-		return (string)new ActionFieldLayout( $input, $button );
 	}
 }
