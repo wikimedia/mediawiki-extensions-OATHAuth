a1526f6fea7159ba32920824259e22716000d493
Roan Kattouw <roan.kattouw@gmail.com>
2026-07-20 20:30:30 -0700

    Automatically set $wgRestrictedGroups if $wgOATHAuthEnforce2FAForAll is set
    Instead of instructing the site admin to set up $wgRestrictedGroups a
    certain way, just do it for them.
    
    Change-Id: Ic89062adc7c0c59e4c3bb7af2a17cd549f16190f

--- src/Hook/RegistrationCallback.php
+++ src/Hook/RegistrationCallback.php
@@ -10,7 +10,22 @@ namespace MediaWiki\Extension\OATHAuth\Hook;
 class RegistrationCallback {
 
 	public static function onRegistration(): void {
+		global $wgOATHAuthEnforce2FAForAll, $wgRestrictedGroups;
+
 		// Following the pattern in TorBlock, this is a string
 		define( 'APCOND_OATH_HAS2FA', 'oath.has_2fa' );
+
+		if ( $wgOATHAuthEnforce2FAForAll ) {
+			// If 2FA is enforced for all users, require 2FA for the 'user' group
+			if ( isset( $wgRestrictedGroups['user']['memberConditions'] ) ) {
+				$wgRestrictedGroups['user']['memberConditions'] = [
+					'&',
+					APCOND_OATH_HAS2FA,
+					$wgRestrictedGroups['user']['memberConditions']
+				];
+			} else {
+				$wgRestrictedGroups['user']['memberConditions'] = [ APCOND_OATH_HAS2FA ];
+			}
+		}
 	}
 }
